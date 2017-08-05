0a64668e308a45f23419e7f34aa3a6399b026276
Dominic Smith <dom@domsmith.co.uk>
2017-08-05 15:33:34 +0100

    Sort timings

--- Frontend/Scripts/slideAnimation.js
+++ Frontend/Scripts/slideAnimation.js
@@ -4,16 +4,16 @@
 
 
 function doAnimation() {
-    var slides = new Array("totalQsos", "qsosByOp", "qsosByOp2", "qsosByOp3", "modeSplit", "news", "news2");
-    // There are 7 slides, each is visible for 8 seconds, depending on the current number of seconds in the time
+    var slides = new Array("totalQsos", "qsosByOp", "qsosByOp2", "qsosByOp3", "modeSplit");
+    // There are 5 slides, each is visible for 12 seconds, depending on the current number of seconds in the time
     var d = new Date();
     var n = d.getSeconds();
     debug = "It is " + n + " seconds";
     // Convert the current number of seconds to a slide:
-    n = Math.floor(n / 8.57);
+    n = Math.floor(n / 12);
     // Hide the old slide
     var oldSlide = n - 1;
-    if (oldSlide < 0) { oldSlide = 6; }
+    if (oldSlide < 0) { oldSlide = 4; }
     debug += "\nRemoving slide " + oldSlide + "\nShowing slide " + n;
     $("#" + slides[oldSlide]).fadeOut("fast", function () {
         // show the new slide
@@ -21,5 +21,5 @@ function doAnimation() {
         $("#" + slides[n]).fadeIn("fast");
     });
     //alert(debug);
-    var animationTimeout = setTimeout(doAnimation, 8570);
+    var animationTimeout = setTimeout(doAnimation, 1200);
 }
