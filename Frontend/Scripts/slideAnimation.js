a46d5cb8a17348843da2fb3e5f29399abb88863c
Dominic Smith <dom@domsmith.co.uk>
2017-08-07 14:21:14 +0100

    And the animation for page 5

--- Frontend/Scripts/slideAnimation.js
+++ Frontend/Scripts/slideAnimation.js
@@ -4,16 +4,16 @@
 
 
 function doAnimation() {
-    var slides = new Array("totalQsos", "qsosByOp", "qsosByOp2", "qsosByOp3", "qsosByOp4", "modeSplit");
-    // There are 6 slides, each is visible for 10 seconds, depending on the current number of seconds in the time
+    var slides = new Array("totalQsos", "qsosByOp", "qsosByOp2", "qsosByOp3", "qsosByOp4", "qsosByOp5", "modeSplit");
+    // There are 7 slides, each is visible for 8 seconds, depending on the current number of seconds in the time
     var d = new Date();
     var n = d.getSeconds();
     debug = "It is " + n + " seconds";
     // Convert the current number of seconds to a slide:
-    n = Math.floor(n / 10);
+    n = Math.floor(n / 8.571);
     // Hide the old slide
     var oldSlide = n - 1;
-    if (oldSlide < 0) { oldSlide = 5; }
+    if (oldSlide < 0) { oldSlide = 6; }
     debug += "\nRemoving slide " + oldSlide + "\nShowing slide " + n;
     $("#" + slides[oldSlide]).fadeOut("fast", function () {
         // show the new slide
